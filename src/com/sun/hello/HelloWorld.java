df9e55d47a379746aae4bc09fa670854fac41885
Vijay Mummaneni <vijaymummaneni@Vijays-MBP.hsd1.ca.comcast.net>
2020-05-01 18:54:43 -0700

    added helloworld sources..

---
+++ src/com/sun/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sun.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<10; i++) {
+		System.out.println("Hellow world.....!");
+		}
+	}
+
+}
